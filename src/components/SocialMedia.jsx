85016c2c529c5bef6c6179d3b070cb7200e40edc
James Dulangon <jamesdulangon@gmail.com>
2022-03-20 17:26:26 +0800

    Add soc meds.

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { BsLinkedin, BsYoutube } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin, BsYoutube, BsGithub } from 'react-icons/bs';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsLinkedin />
+      <a href="https://www.linkedin.com/in/james-steven-dulangon-a53465168/" target="_blank" rel="noreferrer">
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href="https://github.com/JamesDulangon" target="_blank" rel="noreferrer">
+        <BsGithub />
+      </a>
     </div>
     <div>
-      <BsYoutube />
+      <a href="https://www.youtube.com/channel/UCxRXtrc4cDv_6zMrDwDpbUg" target="_blank" rel="noreferrer">
+        <BsYoutube />
+      </a>
     </div>
   </div>
 );
